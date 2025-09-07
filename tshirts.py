e82bab4563b5618e9bc8a7d87cc31369c8f8a8d1
Rushali Parate <rushaliparate@gmail.com>
2025-09-07 18:31:05 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -1,4 +1,3 @@
-
 def size(cms):
     if cms < 38:
         return 'S'
@@ -7,8 +6,15 @@ def size(cms):
     else:
         return 'L'
 
-
+# Weak tests (all pass, even with bug)
 assert(size(37) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
+
+# Strong tests to expose the bug
+def test_size():
+    assert size(38) == 'M', "38 should be M (not S or L)"  # This will fail
+    assert size(42) == 'L', "42 should be L (not M)"       # This will fail if logic is wrong
+
+test_size()
 print("All is well (maybe!)")
